374a443aeaab7a16026b7806cc1e5b6b9440efc7
Juan-Jose-10 <vallecillo.rejua24@zaragoza.salesianos.edu>
2025-04-06 15:05:58 +0200

    Create Led_rojo_y_verde_alternado.ino
    Nuevo trabajo

---
+++ Led_rojo_y_verde_alternado/Led_rojo_y_verde_alternado.ino
@@ -0,0 +1,46 @@
+#include <stdint.h>
+
+// Definir pines para los LEDs
+uint8_t red_led = 2;   // Pin del LED rojo
+uint8_t green_led = 4; // Pin del LED verde
+
+// Variables para temporizadores para el led rojo y verde
+uint64_t previous_millis_red = 0;
+uint64_t previous_millis_green = 0;
+
+//  tiempo para el led rojo y verde
+uint64_t red_interval = 1000;
+uint64_t green_interval = 1000;
+
+void setup() {
+    // Se configuran los pines como salida de cada led
+    Serial.begin(9600);
+    pinMode(red_led, OUTPUT);
+    pinMode(green_led, OUTPUT);
+
+    // Inicializar el estado de cada led, rojo encendido y verde apagado
+    digitalWrite(red_led, HIGH);
+    digitalWrite(green_led, LOW);
+}
+
+void loop() {
+    uint64_t actual_millis = millis();
+
+    if (actual_millis - previous_millis_red > red_interval) { //Programar el momento en el que enciende el led rojo
+        previous_millis_red = millis();
+        if (digitalRead(red_led)) {
+            digitalWrite(red_led, false);
+        } else {
+            digitalWrite(red_led, true);
+        }
+    }
+
+    if (actual_millis - previous_millis_green > green_interval) { //Programar el momento en el que enciende el led verde
+        previous_millis_green = millis();
+        if (digitalRead(green_led)) {
+            digitalWrite(green_led, false);
+        } else {
+            digitalWrite(green_led, true);
+        }
+    }
+}
